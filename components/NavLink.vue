edf0d69a3964601448a7a3812a7162b0d328600e
blanghamm <langham1291@outlook.com>
2023-06-06 23:24:18 +0100

    Changed: sidebar styling and icon set

--- components/NavLink.vue
+++ components/NavLink.vue
@@ -1,6 +1,6 @@
 <template>
-  <NuxtLink class="my-1 p-2 flex items-center justify-center rounded-[8px] hover:bg-gray-200" active-class="bg-gray-200" :to="to">
-    <Icon class="text-gray-600" :name="iconClass" size="22"/>
+  <NuxtLink class="my-1 p-2 flex items-center justify-center rounded-[8px] border border-transparent hover:bg-slate-100 hover:border-slate-100" active-class="bg-slate-100 border !border-slate-200" :to="to">
+    <Icon class="text-slate-800 text-inherit" :name="iconClass" size="22"/>
   </NuxtLink>
 </template>
 <script lang="ts" setup>
--- components/Sidebar.vue
+++ components/Sidebar.vue
@@ -4,12 +4,13 @@
 <template>
   <div class="flex flex-col w-[52px] p-2 h-full justify-between border-r border-gray-200">
     <ul class="flex flex-col items-center">
-      <NavLink to="/" icon-class="ph:house-simple-bold"/>
-      <NavLink to="/flow" icon-class="ph:folder-simple-bold"/>
+      <img class="mb-4" src="/flow-logo-monochrome-light.svg" width="56">
+      <NavLink to="/" icon-class="lucide:layout-dashboard"/>
+      <NavLink to="/flow" icon-class="lucide:folder"/>
     </ul>
     <ul class="flex flex-col items-center">
-      <NavLink to="/user" icon-class="ph:user-gear-bold"/>
-      <NavLink to="/settings" icon-class="ph:gear-six-bold"/>
+      <NavLink to="/user" icon-class="lucide:user-circle-2"/>
+      <NavLink to="/settings" icon-class="lucide:settings"/>
     </ul>
   </div>
 </template>
